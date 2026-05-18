11fc95ac1dfe8255316f06bb48c2a1193126078c
Juanotagua <juan2.otalvaro@ucp.edu.co>
2026-05-18 17:10:55 -0500

    arreglo smtp

--- backend-comisaria/routes/auth.js
+++ backend-comisaria/routes/auth.js
@@ -84,21 +84,15 @@ console.log(passwordCorrecta)
 
 const transporter = nodemailer.createTransport({
 
-  host: 'smtp.gmail.com',
-
-  port: 465,
-
-  secure: true,
+  service: 'gmail',
 
   auth: {
-
     user: process.env.EMAIL_USER,
-
     pass: process.env.EMAIL_PASS
-
   }
 
 })
+
 
 /* =========================
    FORGOT PASSWORD
