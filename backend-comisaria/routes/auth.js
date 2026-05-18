623b46640832f6b5b01b6f2a92f270eec6c54bdb
Juanotagua <juan2.otalvaro@ucp.edu.co>
2026-05-18 17:01:33 -0500

    Cambio a smtp

--- backend-comisaria/routes/auth.js
+++ backend-comisaria/routes/auth.js
@@ -82,17 +82,23 @@ console.log(passwordCorrecta)
    EMAIL
 ========================= */
 
-const transporter =
-  nodemailer.createTransport({
+const transporter = nodemailer.createTransport({
 
-    service: 'gmail',
+  host: 'smtp.gmail.com',
 
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS
-    }
+  port: 465,
 
-  })
+  secure: true,
+
+  auth: {
+
+    user: process.env.EMAIL_USER,
+
+    pass: process.env.EMAIL_PASS
+
+  }
+
+})
 
 /* =========================
    FORGOT PASSWORD
